875d8c41706a5fd2443b5ca26f28241cbb8c1fde
xXMAMBOXx <mar09f@gmail.com>
2014-09-01 17:43:55 -0600

    pruebas con ant

---
+++ Documents/NetBeansProjects/Progra1/TareaCorta-Marvin-Fernández-Coto/src/tareacorta/marvin/fernández/coto/TareaCortaMarvinFernándezCoto.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tareacorta.marvin.fernández.coto;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author xXMAMBOXx
+ */
+public class TareaCortaMarvinFernándezCoto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String t="TOME";
+        Stack l=new Stack();
+        JOptionPane.showInputDialog(args);
+        
+        
+        
+    }
+    
+}
